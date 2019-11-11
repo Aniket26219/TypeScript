ebb5af613b710e7a3c3ced99e1fb61d5772a3f82
ANIKET <aniket98shahane@gmail.com>
2019-11-11 14:41:18 +0530

    optionaling the parameters

--- Practice1.ts
+++ Practice1.ts
@@ -1,7 +1,7 @@
 class Practice{
     x : number;
     y : number;
-    constructor(x : number, y : number){
+    constructor(x? : number, y? : number){
         this.x=x;
         this.y=y;
     }
@@ -9,5 +9,5 @@ class Practice{
         console.log('X = ' + this.x + ' ' + 'Y = ' + this.y);
     }
 }
-let prac = new Practice(3,5);
+let prac = new Practice();
 prac.draw();
